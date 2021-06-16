df14c5f2440207e019f5332f9ebb019344f3a0dc
davidmacer <jdavid.macer@gmail.com>
2021-06-16 17:39:54 -0600

    Cambia mensaje que se muestra

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.send('¡Qué onda, piojos locos!'));
 
 app.listen(3000);
 console.log('Server on port', 3000);
